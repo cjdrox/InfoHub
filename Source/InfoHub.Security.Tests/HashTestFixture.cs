393c9b335593183ff0bf2c492b397c6ed84eac46
Chameera <cjdrox@gmail.com>
2013-06-09 02:23:51 +0530

    Added test cases for the Extension classes

---
+++ Source/InfoHub.Security.Tests/HashTestFixture.cs
@@ -0,0 +1,51 @@
+﻿using InfoHub.Security.Models;
+using NUnit.Framework;
+
+namespace InfoHub.Security.Tests
+{
+    [TestFixture]
+    public class HashTestFixture
+    {
+        [Test]
+        public void MD5HashTest()
+        {
+            // Setup
+            const string testString = "test string";
+            const string expected = "6f8db599de986fab7a21625b7916589c";
+
+            // Execute
+            var result = testString.MD5Hash().ToLower();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void SHA1HashTest()
+        {
+            // Setup
+            const string testString = "test string";
+            const string expected = "661295c9cbf9d6b2f6428414504a8deed3020641";
+
+            // Execute
+            var result = testString.SHA1Hash().ToLower();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ROT13Test()
+        {
+            // Setup
+            const string testString = "Lo, A Test String";
+            const string expected = "Yb, N Grfg Fgevat";
+
+            // Execute
+            var result = testString.ROT13();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- Source/InfoHub.Security/Models/Transform.cs
+++ Source/InfoHub.Security/Models/Transform.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Security.Cryptography;
+using System.Text;
 using InfoHub.Security.Interfaces;
 
 namespace InfoHub.Security.Models
@@ -7,7 +8,25 @@ namespace InfoHub.Security.Models
     {
         public static string ROT13(this string str)
         {
-            throw new NotImplementedException();
+            var array = str.ToCharArray();
+            for (var i = 0; i < array.Length; i++)
+            {
+                int number = array[i];
+
+                if (number >= 'a' && number <= 'z')
+                {
+                    if (number > 'm') { number -= 13;}
+                    else { number += 13; }
+                }
+                else if (number >= 'A' && number <= 'Z')
+                {
+                    if (number > 'M') { number -= 13; }
+                    else { number += 13; }
+                }
+
+                array[i] = (char)number;
+            }
+            return new string(array);
         }
 
         public static string Encrypt(this string str, ICryptor cryptor)
@@ -20,9 +39,38 @@ namespace InfoHub.Security.Models
             return cryptor.Decrypt(str);
         }
 
-        public static string Hash(this string str)
+        public static string MD5Hash(this string str)
         {
-            throw new NotImplementedException();
+            var md5 = MD5.Create();
+            
+
+            var inputBytes = Encoding.ASCII.GetBytes(str);
+            var hash = md5.ComputeHash(inputBytes);
+
+            return hash.ToHexString();
+        }
+
+        public static string SHA1Hash(this string str)
+        {
+            var inputBytes = Encoding.ASCII.GetBytes(str);
+            
+            SHA1 sha = new SHA1CryptoServiceProvider();
+
+            var result = sha.ComputeHash(inputBytes);
+
+            return result.ToHexString();
+        }
+
+        public static string ToHexString(this byte[] bytes)
+        {
+            var sb = new StringBuilder();
+
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("X2"));
+            }
+
+            return sb.ToString();
         }
     }
 }
