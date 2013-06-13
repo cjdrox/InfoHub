5fe2a96cd1872a2578de8653fb9ee66a32f8fa62
Chameera <cjdrox@gmail.com>
2013-06-13 22:25:51 +0530

    Configuration class tested

--- Source/InfoHub.ORM.Tests/ORMTestFixture.cs
+++ Source/InfoHub.ORM.Tests/ORMTestFixture.cs
@@ -1,12 +1,41 @@
-﻿using NUnit.Framework;
+﻿using InfoHub.ORM.Interfaces;
+using InfoHub.ORM.Models;
+using NUnit.Framework;
 
 namespace InfoHub.ORM.Tests
 {
-    /// <summary>
-    /// Summary description for UnitTest1
-    /// </summary>
     [TestFixture]
     public class ORMTestFixture
     {
+        [Test]
+        public void TestConfiguration()
+        {
+            // Setup
+            IConfiguration configuration = new Configuration("localhost", "blah", "3308", "sa", "sa");
+
+            // Execute
+            var host = configuration.Host;
+            var database = configuration.Database;
+            var port = configuration.Port;
+            var user = configuration.Username;
+            var pass = configuration.Password;
+
+            var constring = string.Format("SERVER={0}; DATABASE={1}; UID= {2}; PASSWORD={3};",
+                    host, database, user, pass);
+
+            // Assert
+            Assert.IsNotNull(configuration);
+            Assert.AreEqual("localhost", host);
+            Assert.AreEqual("blah", database);
+            Assert.AreEqual("3308", port);
+            Assert.AreEqual("sa", user);
+            Assert.AreEqual("sa", pass);
+            Assert.AreEqual(constring, configuration.ConnectionString);
+            Assert.AreEqual(true, configuration.IsValid);
+
+            // Invalidate and assert
+            configuration.Dispose();
+            Assert.AreEqual(false, configuration.IsValid);
+        }
     }
 }
