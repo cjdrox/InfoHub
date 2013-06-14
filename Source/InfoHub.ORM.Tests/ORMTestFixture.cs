f85c1ca3cd2f4828934243568efacdc139dfe209
Chameera <cjdrox@gmail.com>
2013-06-14 20:55:59 +0530

    Table tests added

--- Source/InfoHub.ORM.Tests/ORMTestFixture.cs
+++ Source/InfoHub.ORM.Tests/ORMTestFixture.cs
@@ -1,4 +1,5 @@
-﻿using InfoHub.ORM.Interfaces;
+﻿using System.Linq;
+using InfoHub.ORM.Interfaces;
 using InfoHub.ORM.Models;
 using NUnit.Framework;
 
@@ -36,6 +37,58 @@ namespace InfoHub.ORM.Tests
             // Invalidate and assert
             configuration.Dispose();
             Assert.AreEqual(false, configuration.IsValid);
+        }
+
+        [Test]
+        public void TestTable()
+        {
+            // Setup 
+            var tableOne = new Table("One");
+            var tableTwo = new Table("Two").WithName("Two");
+            var tableThree = new Table().WithName("Three").WithColumn<string>("ID");
+            var tableFour = new Table("Blah").WithName("Four").WithColumn<string>("ID", 10);
+            var tableFive = new Table("Blah").WithName("Five").WithColumn<string>("ID", 10, true);
+
+            // Execute
+            var nameOne = tableOne.TableName;
+            var nameTwo = tableTwo.TableName;
+            var nameThree = tableThree.TableName;
+            var nameFour = tableFour.TableName;
+            var nameFive = tableFive.TableName;
+
+            // Assert
+            Assert.IsNotNull(tableOne);
+            Assert.IsNotNull(tableTwo);
+            Assert.IsNotNullOrEmpty(nameOne);
+            Assert.AreEqual("One", nameOne);
+            Assert.IsNotNullOrEmpty(nameTwo);
+            Assert.AreEqual("Two", nameTwo);
+            Assert.IsNotNullOrEmpty(nameTwo);
+            Assert.AreEqual("Three", nameThree);
+            Assert.IsNotNullOrEmpty(nameFour);
+            Assert.AreEqual("Four", nameFour);
+            Assert.IsNotNullOrEmpty(nameFive);
+            Assert.AreEqual("Five", nameFive);
+
+            Assert.IsNotNull(tableThree.ColumnTypes);
+            Assert.AreEqual(1, tableThree.ColumnTypes.Count);
+            Assert.AreEqual("ID", tableThree.ColumnTypes.First().Key);
+            Assert.AreEqual("String", tableThree.ColumnTypes.First().Value.Type.Name);
+            Assert.IsFalse(tableThree.ColumnTypes.First().Value.IsPrimary);
+
+            Assert.IsNotNull(tableFour.ColumnTypes);
+            Assert.AreEqual(1, tableFour.ColumnTypes.Count);
+            Assert.AreEqual("ID", tableFour.ColumnTypes.First().Key);
+            Assert.AreEqual("String", tableFour.ColumnTypes.First().Value.Type.Name);
+            Assert.AreEqual(10, tableFour.ColumnTypes.First().Value.Length);
+            Assert.IsFalse(tableFour.ColumnTypes.First().Value.IsPrimary);
+
+            Assert.IsNotNull(tableFive.ColumnTypes);
+            Assert.AreEqual(1, tableFive.ColumnTypes.Count);
+            Assert.AreEqual("ID", tableFive.ColumnTypes.First().Key);
+            Assert.AreEqual("String", tableFive.ColumnTypes.First().Value.Type.Name);
+            Assert.AreEqual(10, tableFive.ColumnTypes.First().Value.Length);
+            Assert.IsTrue(tableFive.ColumnTypes.First().Value.IsPrimary);
         }
     }
 }
